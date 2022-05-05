df778735f44771cca7b8bcf1ca4bdff2518c5502
18ec058 <73119620+18ec058@users.noreply.github.com>
2022-05-05 21:26:41 +0530

    Create constant.js

---
+++ Fixed/constant.js
@@ -0,0 +1,5 @@
+export const COUNTRY_API = "https://restcountries.com/v3.1/name";
+
+const ACCESS_KEY = "0d2832004510d0f75a2ccd52b2f06ab4";
+
+export const WEATHER_API = `http://api.weatherstack.com/current?access_key=${ACCESS_KEY}`;
